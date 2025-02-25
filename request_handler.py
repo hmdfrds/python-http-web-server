606560245f8dfd34cd5d0366484d8bda2da4026e
Firdaus <firdaus1337@gmail.com>
2025-02-26 00:05:42 +0800

    Implement TCP socket to listen and for incomming connections

---
+++ request_handler.py
@@ -0,0 +1,2 @@
+class HTTPRequestHandler:
+    pass
--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 import json
 import os
 import sys
+import socket
+import threading
+from request_handler import HTTPRequestHandler
 from logger import Logger
 
 
 def load_config(config_file="config.json"):
+    """Loads and validates the configuration from a JSON file.
+
+    Args:
+        config_file (str, optional): Path to the confiugration file. Defaults to "config.json".
+
+    Raises:
+        FileNotFoundError: If the confiugration file does not exist.
+
+    Returns:
+        dict: Dictionary of config data.
+    """
 
     if not os.path.exists(config_file):
         raise FileNotFoundError(f"Configuration file'{config_file}' not found.")
@@ -46,18 +60,58 @@ def load_config(config_file="config.json"):
     return config
 
 
+def start_server(config, logger):
+    """Set up the TCP socket, listen for incoming connections, and spawn a new thread for each connection.
+
+    Args:
+        config (dict): Configuration file loaded when the server start.
+        logger (Logger): log instance.
+    """
+    host = config["host"]
+    port = config["port"]
+    max_threads = config["max_threads"]
+
+    # Create a TCP socket. (Address Family - Internet) (Socket Type - Stream-based)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_socket.bind((host, port))
+        server_socket.listen(max_threads)
+        print(f"HTTP Server is listening on {host}:{port}")
+        logger.log(f"Server started on {host}:{port}")
+
+        # Main accept-loop
+        while True:
+            try:
+                client_conn, client_addr = server_socket.accept()
+                print(f"Accepted connection from {client_addr[0]}")
+                # Spawn a new thread to handler the client connection.
+                thread = threading.Thread(
+                    target=HTTPRequestHandler.handle_client,
+                    args=(client_conn, client_addr, config, logger),
+                    daemon=True,
+                )
+                thread.start()
+            except Exception as e:
+                logger.log_error(f"Error handling connection: {e}")
+    except Exception as e:
+        logger.log_error(f"Server socket error: {e}")
+    finally:
+        server_socket.close()
+
+
 if __name__ == "__main__":
 
     try:
         config = load_config()
-        logger = Logger(config["log_file"])
-        logger.start_periodic_stats()  # Start stats logging every 60 seconds
-
         print("Configuration loaded successfully:")
         for key, value in config.items():
             print(f"    {key}: {value}")
-
     except Exception as e:
-        logger.log_error(e)
         print(f"Error loading configuration: {e}")
         sys.exit(1)
+
+    logger = Logger(config["log_file"])
+    logger.start_periodic_stats()  # Start stats logging every 60 seconds
+
+    # Start the HTTP server
+    start_server(config, logger)
